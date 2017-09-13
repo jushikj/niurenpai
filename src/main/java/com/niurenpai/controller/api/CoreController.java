a6f45c6e2d8327ecae046cce9ea1b73c2f088bc6
root <doitman2010@163.com>
2017-09-13 23:52:48 +0800

    add api

--- src/main/java/com/niurenpai/controller/api/CoreController.java
+++ src/main/java/com/niurenpai/controller/api/CoreController.java
@@ -4,6 +4,8 @@ import com.niurenpai.service.CoreService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -19,6 +21,22 @@ public class CoreController {
     Logger logger = LoggerFactory.getLogger(getClass());
     @Resource
     CoreService coreService;
+
+    @RequestMapping("/niurenpai/index")
+    @ResponseBody
+    public String index() {
+
+        //分页查询竞拍计划列表
+
+        return null;
+    }
+
+    @RequestMapping("/niurenpai/niuren/detail")
+    @ResponseBody
+    public String niurenDetail() {
+        return null;
+    }
+
 
 
 }
